ba8cadd15f69fa56dedb90628a38e900b77aff33
Niall Molloy <gunjam@gmail.com>
2024-06-10 13:52:22 +0100

    perf: improve speed of govukButton()

---
+++ test/utils/attribute.test.js
@@ -0,0 +1,27 @@
+import { equal } from 'node:assert/strict'
+import { describe, it } from 'node:test'
+import attribute from '../../utils/attribute.js'
+
+describe('attribute', () => {
+  it('render attribute if value is not undefined', () => {
+    equal(attribute('id', 'test'), ' id="test"')
+    equal(attribute('id', true), ' id="true"')
+    equal(attribute('id', false), ' id="false"')
+    equal(attribute('id', 0), ' id="0"')
+    equal(attribute('id', 1), ' id="1"')
+  })
+
+  it('render empty attribute if value is empty string or null', () => {
+    equal(attribute('id', null), ' id=""')
+    equal(attribute('id', ''), ' id=""')
+  })
+
+  it('escape HTML in input strings', () => {
+    equal(attribute('id', '<>'), ' id="&lt;&gt;"')
+    equal(attribute('<>', 'test'), ' &lt;&gt;="test"')
+  })
+
+  it('returns empty string if value is undefined', () => {
+    equal(attribute('id', undefined), '')
+  })
+})
