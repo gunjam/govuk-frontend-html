72c77b7a1aeded53dc447acf8f3ad3427e3758f9
Niall Molloy <gunjam@gmail.com>
2024-06-11 10:03:56 +0100

    feat: add govukErrorMessage() component

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import govukButton from './components/button/button.js'
 import govukDetails from './components/details/details.js'
+import govukErrorMessage from './components/error-message/error-message.js'
 import govukHint from './components/hint/hint.js'
 import govukLabel from './components/label/label.js'
 import govukSkipLink from './components/skip-link/skip-link.js'
 import govukWarningText from './components/warning-text/warning-text.js'
 
-export { govukButton, govukDetails, govukHint, govukLabel, govukSkipLink, govukWarningText }
+export {
+  govukButton,
+  govukDetails,
+  govukErrorMessage,
+  govukHint,
+  govukLabel,
+  govukSkipLink,
+  govukWarningText
+}
